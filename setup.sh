43a948a88cfb0ece6489f06a786634cf23f9ba0f
PastaMasta <pasta.masta2902@gmail.com>
2016-04-05 20:26:22 +0100

    Moved config

--- setup.sh
+++ setup.sh
@@ -1,16 +1,21 @@
 #! /bin/bash
 
-# Setup symlinks in ~/ to these config files
+# Setup symlinks in ~/ for dotfiles
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/dotfiles"
 configs=`ls -A ${DIR} | grep -vE ".git$|setup.sh"`
 
 for config in ${configs} ; do
 
-  if ln -s ${DIR}/${config} ~/${config} ; then
+  if ln -sf ${DIR}/${config} ~/.${config} ; then
     echo "Linked ${DIR}/${config} to ~/${config}"
   fi
 
 done
+
+# Setup config
+if ln -sf ${DIR}/config ~/.config ; then
+    echo "Linked ${DIR}/config to ~/.config"
+fi
 
 exit $?
