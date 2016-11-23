d75ff7dffe1c52ade28174bfda8e7edfb66ae342
PastaMasta <pasta.masta2902@gmail.com>
2016-11-23 21:53:29 +0000

    Initial borg script

---
+++ scripts/borg-backup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+#
+# Uses borg to backup a set of directories to a borg repository
+# specified in a configuration file.
+#
+# Default config file:
+config_file='~/.config/borg/backup.conf'
+
+#[[ $# -gt 0 ]] && config_file=$1
+
+function usage {
+  echo "Usage:"
+  echo "$* /path/to/config_file"
+  exit 1
+}
+
+echo "Config file is - ${config_file}"
+if [[ ! -r ${config_file} ]] ; then
+  . ${config_file}
+else
+  echo "Couldn't read ${config_file} !"
+  usage $0
+fi
+
+
+borg create -C none ${REPOSITORY}::'{hostname}-{now:%Y-%m-%d_%H:%M:%S}' ${DIRS} 
+# borg prune -v ${REPOSITORY} --prefix '{hostname}-' --keep-within=7d --keep-weekly=4 --keep-monthly=6
