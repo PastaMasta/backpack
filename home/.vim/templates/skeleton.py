57040a52fa4cbbc9cb218e8907d3706170e89694
PastaMasta <pasta.masta2902@gmail.com>
2022-01-24 17:15:29 +0000

    Added some default parser options to python

--- home/.vim/templates/skeleton.py
+++ home/.vim/templates/skeleton.py
@@ -1,4 +1,23 @@
 #! /usr/bin/env python3
 
+import pdb
+import argparse
+from halo import Halo
+
+def run():
+    pdb.set_trace()
+    return "hello"
+
 if __name__ == "__main__":
-    import pdb; pdb.set_trace()
+
+    parser = argparse.ArgumentParser(description='Do something')
+    parser.add_argument("--no-spin",help="Don't spin the spinny spinner",action="store_true")
+    args = parser.parse_args()
+
+    if args.no_spin:
+        results = run()
+    else:
+        with Halo(text='', spinner='line'):
+            results = run()
+
+    pdb.set_trace()
