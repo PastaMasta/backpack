465bea316086dc32f1588b72c5133557964b0a23
PastaMasta <pasta.masta2902@gmail.com>
2022-03-22 18:26:35 +0000

    Changed to cli_args as args is a pdb command

--- home/.vim/templates/skeleton.py
+++ home/.vim/templates/skeleton.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Do something')
     parser.add_argument("--no-spin",help="Don't spin the spinny spinner",action="store_true")
-    args = parser.parse_args()
+    cli_args = parser.parse_args()
 
-    if args.no_spin:
+    if cli_args.no_spin:
         results = run()
     else:
         with Halo(text='', spinner='line'):
