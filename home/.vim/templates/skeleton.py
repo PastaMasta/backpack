ce50ab254fbe4105dfe9b29711999fac6b9ac02c
PastaMasta <pasta.masta2902@gmail.com>
2023-08-07 14:50:21 +0100

    Added more args to skeleton.py

--- home/.vim/templates/skeleton.py
+++ home/.vim/templates/skeleton.py
@@ -15,9 +15,14 @@ if __name__ == "__main__":
     log = logging.getLogger(__name__)
     log.debug("Starting: "+__name__)
 
-    parser = argparse.ArgumentParser(description='Do something')
+    parser = argparse.ArgumentParser(
+            description='Do something',
+            epilog="Have a nice day!"
+            )
     parser.add_argument("--no-spin",help="Don't spin the spinny spinner",action="store_true")
     parser.add_argument("--truefalse",help="true / false switch",action="store_true")
+    parser.add_argument("--choice", type=int, choices=[0, 1, 2], help="Choice of 1 2 or 3",action="store")
+    parser.add_argument("--store", type=str, help="Store a value",action="store")
     parser.add_argument("--list",metavar='item',nargs='+',help="List of args into an array",type=str)
     cli_args = parser.parse_args()
 
